dfcc2aab521999f0346a91947cdfee3c0fac6c99
Fraloox <lautarofranco75@gmail.com>
2022-09-30 15:28:29 -0300

    ya loguea con google, falta manipular mejor los errores y el ingreso

--- app/src/main/java/com/lautaro/log_insortear/MainActivity.kt
+++ app/src/main/java/com/lautaro/log_insortear/MainActivity.kt
@@ -1,51 +1,18 @@
 package com.lautaro.log_insortear
 
-import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shadow
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.core.app.ActivityCompat.startActivityForResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.tasks.Task
 import com.lautaro.log_insortear.disenoUI.LoginScreen
 import com.lautaro.log_insortear.disenoUI.MainViewModel
 import com.lautaro.log_insortear.ui.theme.LoginSortearTheme
@@ -77,12 +44,28 @@ class MainActivity : ComponentActivity() {
                 }
 
                 LoginScreen(isLoading){
-                    viewModel.loginWithGoogle()
+                    viewModel.loginWithGoogle(this@MainActivity)
                 }
 
 
             }
         }
+    }
+}
+
+@Override
+fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) { //codigo de google
+
+    onActivityResult(requestCode, resultCode, data)
+
+    val viewModel: MainViewModel = MainViewModel()
+
+    // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+    if (requestCode == 1) {
+        // The Task returned from this call is always completed, no need to attach
+        // a listener.
+        val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+        viewModel.finishLogin(task)
     }
 }
 
--- app/src/main/java/com/lautaro/log_insortear/disenoUI/MainViewModel.kt
+++ app/src/main/java/com/lautaro/log_insortear/disenoUI/MainViewModel.kt
@@ -1,12 +1,20 @@
 package com.lautaro.log_insortear.disenoUI
 
+import android.app.Activity
+import android.content.Intent
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import java.lang.Math.random
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.lautaro.log_insortear.R
+
 
 class MainViewModel: ViewModel() { //Esta clase es para manejar los estados
 
@@ -20,25 +28,71 @@ class MainViewModel: ViewModel() { //Esta clase es para manejar los estados
 
     fun hasErrors(): LiveData<Boolean> = hasErrors
 
-    fun loginWithGoogle(){
+    fun loginWithGoogle(activity: Activity){
 
         //avisa que el valor cambia
         isLoading.postValue(true)
 
-        viewModelScope.launch {
-            delay(5000) //delay de 5 segundos
 
-            isLoading.postValue(false)
+        //Codigo de google https://developers.google.com/identity/sign-in/android/sign-in
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(activity.getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
 
-            if (random() < 0.4){
-                hasErrors.postValue(true)
-            }
-        }
+        val client = GoogleSignIn.getClient(activity, gso);
+
+        val signInIntent: Intent = client.signInIntent //delega la tarea a la app correspondiente
+        activity.startActivityForResult(signInIntent, 1)
 
     }
 
     fun clearErrors() {
         hasErrors.postValue(false)
+    }
+
+    fun finishLogin(accountTask: Task<GoogleSignInAccount>) {
+
+        try {
+            val account: GoogleSignInAccount = accountTask.getResult(ApiException::class.java)
+
+            // Signed in successfully, show authenticated UI.
+            account?.idToken?.let { token ->
+
+                //instancia para trabajar con firebase
+                val auth = FirebaseAuth.getInstance()
+
+                //El "GoogleAuthProvider" se puede poner dependiendo el servicio que queremos logear
+                val credential = GoogleAuthProvider.getCredential(token, null)
+
+                auth.signInWithCredential(credential)
+                    .addOnCompleteListener{ task ->
+
+                        var user = auth.currentUser
+
+                        if (task.isSuccessful){
+                            Log.d("ACA", "Ingreso ${user?.displayName}")
+                        }else{
+                            Log.d("ACA", "Failed ${task.exception?.message}")
+                            hasErrors.postValue(true)
+                        }
+                        isLoading.postValue(false)
+
+
+                }
+
+            }
+
+        } catch (e: ApiException) {
+
+            Log.d("ACA","Failed ${e.message}")
+
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            hasErrors.postValue(true)
+            isLoading.postValue(false)
+        }
+
     }
 
 }
